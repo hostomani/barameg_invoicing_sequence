5c56eca199b3af5a72aa856e992f2daeb6ae01e4
hostomani <hostomani@gmail.com>
2022-03-18 01:40:05 +0200

    fix invoice payment

--- models/models.py
+++ models/models.py
@@ -8,11 +8,13 @@ class AccountMove(models.Model):
 
     @api.model
     def create(self, values):
+        print(values)
         name = ''
         ct = datetime.datetime.now()
         for segment in str(ct).split(' ')[0].split('-'):
             name += segment
-        name += str(values['partner_id'])
+        name += str(values['partner_id']) if not values['invoice_user_id'] else str(values['invoice_user_id'])
+        print(name)
         for segment in str(ct).split(' ')[1].split('.')[0].split(':'):
             name += segment
         values['name'] = name
